e95da3d514e9766f377b6723c80f18dc54eb0b9a
Jan P. Pasierb <janek.pasierb@gmail.com>
2021-02-20 01:24:58 +0000

    - Status now only shows load avgs and time - gitignore added for executables

--- dwmstatus.c
+++ dwmstatus.c
@@ -17,9 +17,7 @@
 
 #include <X11/Xlib.h>
 
-char *tzargentina = "America/Buenos_Aires";
-char *tzutc = "UTC";
-char *tzberlin = "Europe/Berlin";
+char *tzlondon = "Europe/London";
 
 static Display *dpy;
 
@@ -64,6 +62,7 @@ mktimes(char *fmt, char *tzname)
 	tim = time(NULL);
 	timtm = localtime(&tim);
 	if (timtm == NULL)
+
 		return smprintf("");
 
 	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
@@ -164,58 +163,26 @@ getbattery(char *base)
 	return smprintf("%.0f%%%c", ((float)remcap / (float)descap) * 100, status);
 }
 
-char *
-gettemperature(char *base, char *sensor)
-{
-	char *co;
-
-	co = readfile(base, sensor);
-	if (co == NULL)
-		return smprintf("");
-	return smprintf("%02.0f°C", atof(co) / 1000);
-}
-
 int
 main(void)
 {
 	char *status;
 	char *avgs;
-	char *bat;
-	char *bat1;
-	char *tmar;
-	char *tmutc;
 	char *tmbln;
-	char *t0, *t1, *t2;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
 		return 1;
 	}
 
-	for (;;sleep(60)) {
+	for (;;sleep(1)) {
 		avgs = loadavg();
-		bat = getbattery("/sys/class/power_supply/BAT0");
-		bat1 = getbattery("/sys/class/power_supply/BAT1");
-		tmar = mktimes("%H:%M", tzargentina);
-		tmutc = mktimes("%H:%M", tzutc);
-		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzberlin);
-		t0 = gettemperature("/sys/devices/virtual/hwmon/hwmon0", "temp1_input");
-		t1 = gettemperature("/sys/devices/virtual/hwmon/hwmon2", "temp1_input");
-		t2 = gettemperature("/sys/devices/virtual/hwmon/hwmon4", "temp1_input");
+		tmbln = mktimes("%W %a %d %b %H:%M %Z %Y", tzlondon);
 
-		status = smprintf("T:%s|%s|%s L:%s B:%s|%s A:%s U:%s %s",
-				t0, t1, t2, avgs, bat, bat1, tmar, tmutc,
-				tmbln);
+		status = smprintf("Load averages: %s    |    Time: %s",avgs,tmbln);
 		setstatus(status);
 
-		free(t0);
-		free(t1);
-		free(t2);
 		free(avgs);
-		free(bat);
-		free(bat1);
-		free(tmar);
-		free(tmutc);
 		free(tmbln);
 		free(status);
 	}
